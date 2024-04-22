2972f94416fc3e6591e08ff7e4a5de94d949c4df
denysbirak <denys.birak.pz.2020@lpnu.ua>
2024-04-22 04:53:48 +0300

    Complete all tasks of "String, RegExp, DateTime"

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-class Animal {
-    constructor(animalName) {
-        this.animalName = animalName;
-    }
+// Task 1
+let str1 = 'ahb acb aeb aeeb adcb axeb';
+const re1 = /a.b/g;
+console.log(str1.match(re1));
 
-    move() {
-        console.log(`${this.animalName} is moving`);
-    }
-}
+// Task 2
+let str2 = '2 + 3 223 2223';
+const re2 = /\b2\s*\+\s*3\b/;
+console.log(str2.match(re2)[0]);
 
-class Cat extends Animal {
-    constructor(animalName) {
-        super(animalName);
-    }
-}
+// Task 3
+const currentDate = new Date();
+const day = currentDate.getDate();
+const month = currentDate.getMonth() + 1;
+const year = currentDate.getFullYear();
 
-const luna = new Cat('luna');
-luna.move();
+console.log(`Day: ${day}`);
+console.log(`Month: ${month}`);
+console.log(`Year: ${year}`);
